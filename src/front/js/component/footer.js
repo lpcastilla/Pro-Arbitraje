6fe2d6acc76d4de20666cd1557202b78f8bf83e4
Leonardo Pérez-Castilla <128614607+lpcastilla@users.noreply.github.com>
2023-09-21 22:16:46 +0000

    se creo el landing page

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
+import "../../styles/footer.css";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="footer mt-auto py-3">
+		<div className="container" style={{ width: "85%" }}>
+			<div className="footerLogo mb-3">
+				<img
+					src="https://res.cloudinary.com/dronv3ars/image/upload/v1695323771/Pro-Arbitraje-removebg-preview_w9qix2.png"
+					className="logo-img-footer img-fluid"
+					alt="Logo"
+				/>
+			</div>
+			<div className="container text-start">
+				<div className="row mb-5">
+					<div className="col-4">
+						<div className="row opacity-50 mt-3 fs-sm">Rankings</div>
+						<div className="row mt-2 fs-sm">Encuentra un bufete</div>
+						<div className="row mt-2 fs-sm">Encuentra un abogado</div>
+					</div>
+					<div className="col-4">
+						<div className="row opacity-50 mt-3 fs-sm">Compañía</div>
+						<div className="row mt-2 fs-sm">Sobre Nosotros</div>
+						<div className="row mt-2 fs-sm">Contáctanos</div>
+					</div>
+					<div className="col-4">
+						<div className="row opacity-50 mt-3 fs-sm">Dirección</div>
+						<div className="row mt-2 fs-sm">Avenida Rómulo Gallegos</div>
+						<div className="row mt-2 fs-sm">Torre Poliprima, Piso 6</div>
+						<div className="row mt-2 fs-sm">Caracas, Venezuela</div>
+					</div>
+				</div>
+			</div>
+			<div className="row mt-5">
+				<div className="col-6">
+					<p>
+						<i className="fa-regular fa-copyright" /> Copyright 2023 Pro-Arbitraje | Términos y condiciones	
+					</p>
+				</div>
+				<div className="iconos col-6 text-end">
+					<i className="fa-brands fa-linkedin fa-lg ms-3"></i>
+					<i className="fa-brands fa-instagram fa-lg ms-3"></i>
+					<i className="fa-brands fa-square-x-twitter fa-lg ms-3"></i>
+				</div>
+			</div>
+		</div>
 	</footer>
 );
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,17 +1,59 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+		<nav className="navbar navbar-expand-lg main-header p-0 px-lg-5 mb-70">
+			<div className="container-fluid containerDeNavbarHome">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">
+						<img
+							src="https://res.cloudinary.com/dronv3ars/image/upload/v1695323771/Pro-Arbitraje-removebg-preview_w9qix2.png"
+							className="logo-img img-fluid"
+							alt="Logo"
+						/>
+					</span> 
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+				<a
+					className="navbar-toggler"
+					type="link"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNav"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+					>
+					<span className="landingButton"><i className="fa-solid fa-bars fa-lg"></i></span>
+				</a>
+				<div className="collapse navbar-collapse ml-auto" id="navbarNav">
+					<ul className="navbar-nav landingNav">
+						<li className="nav-item itemNav">
+							<Link
+								className="nav-link active text-decoration-none nav-link-black"
+								aria-current="page"
+								to="/Nosotros"
+								>
+									Nosotros
+							</Link>
+						</li>
+						<li className="nav-item itemNav">
+							<Link
+								className="nav-link text-decoration-none nav-link-black"
+								to="/Login"
+								>
+									Iniciar sesión
+							</Link>
+						</li>
+						<li className="nav-item itemNav">
+							<Link
+								className="nav-link text-decoration-none nav-link-black"
+								to="/Registrarse"
+							>
+								Registrarse
+							</Link>
+						</li>
+					</ul>
 				</div>
 			</div>
 		</nav>
