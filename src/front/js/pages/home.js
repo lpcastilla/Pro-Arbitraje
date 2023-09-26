23abb4c083331f794db75c4b0e29f054f59135f2
Leonardo Pérez-Castilla <128614607+lpcastilla@users.noreply.github.com>
2023-09-26 22:13:35 +0000

    se incorporo imagen y menu de busqueda al landing

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,15 +1,34 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			
+		<div className="text-center position-relative">
+			<img
+				src="https://res.cloudinary.com/dronv3ars/image/upload/v1695764739/Orange_and_Blue_Corporative_Modern_Presentation_tnzizq.jpg"
+				className="landing-img img-fluid"
+				alt="Start"
+			/>
+
+			<div className="title">
+				<h1>Encuentra los mejores abogados.</h1>	
+			</div>	
+
+			<div className="search-bar">
+				<div className="input-group">
+					<input type="text" className="form-control" placeholder="Buscar..." aria-label="Buscar" aria-describedby="basic-addon2" />
+					<div className="input-group-append">
+						<button className="btn btn-secondary" type="button">
+							Buscar
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
+
 
